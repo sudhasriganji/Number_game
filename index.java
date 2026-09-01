2032bd51a36a50c8af4d976dea782f76591867b1
sudhasriganji <sudhasri.ganji@gmail.com>
2026-09-01 22:32:02 +0530

    Update index.java

--- index.java
+++ index.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Number_Game{
+public class Decodelabs_java_P1{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         Random random =new Random();
